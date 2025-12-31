66a54e2988f06545fd51941ee615b36080b40a7b
facusteckler86 <facundomsteckler@gmail.com>
2025-12-31 18:03:13 -0300

    armo menu y titulo provisorio del main

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
+   
 })
 export class App {
   protected readonly title = signal('practica2026');
 }
+
+
